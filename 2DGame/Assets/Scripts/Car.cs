48bd7f388c34b133ca457b666f3b4b203f6de601
KID <50666207+KID421@users.noreply.github.com>
2020-11-14 15:28:40 +0800

    認識欄位

---
+++ 2DGame/Assets/Scripts/Car.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    // 單行註解：紀錄、說明、開發者名稱..
+
+    // 資料 = C# 欄位 (Field)
+    // 語法：
+    // 類型 名稱；
+
+    // 四大基本類型：整數、浮點數、字串、布林值
+    int cc;
+    float weight;
+    string brand;
+    bool haveWindow;
+}
