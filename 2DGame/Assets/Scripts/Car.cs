2efc1147b0c2e543703d99981b839714fc833bd6
KID <50666207+KID421@users.noreply.github.com>
2020-11-14 15:34:30 +0800

    欄位語法

--- 2DGame/Assets/Scripts/Car.cs
+++ 2DGame/Assets/Scripts/Car.cs
@@ -6,11 +6,12 @@ public class Car : MonoBehaviour
 
     // 資料 = C# 欄位 (Field)
     // 語法：
-    // 類型 名稱；
+    // 類型 名稱 (指定 值)；
 
     // 四大基本類型：整數、浮點數、字串、布林值
-    int cc;
-    float weight;
-    string brand;
-    bool haveWindow;
+    
+    int cc = 2000;
+    float weight = 1500.5f;         // 小數點後面必須加上 f 或 F
+    string brand = "BMW";           // 文字必須使用雙引號
+    bool haveWindow = true;         // 是 true、否 false
 }
