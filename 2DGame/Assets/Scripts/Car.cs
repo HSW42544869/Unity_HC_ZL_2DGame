a499fb455a4108b09106fe415cb714eeaee160c9
KID <50666207+KID421@users.noreply.github.com>
2020-11-14 15:48:59 +0800

    修飾詞

--- 2DGame/Assets/Scripts/Car.cs
+++ 2DGame/Assets/Scripts/Car.cs
@@ -6,12 +6,16 @@ public class Car : MonoBehaviour
 
     // 資料 = C# 欄位 (Field)
     // 語法：
-    // 類型 名稱 (指定 值)；
+    // 修飾詞 類型 名稱 (指定 值)；
+
+    // 修飾詞 - 定義資料的權限
+    // 私人 private (預設值)  - 不顯示在屬性面板上
+    // 公開 public            - 顯示在屬性面板上
 
     // 四大基本類型：整數、浮點數、字串、布林值
-    
-    int cc = 2000;
-    float weight = 1500.5f;         // 小數點後面必須加上 f 或 F
-    string brand = "BMW";           // 文字必須使用雙引號
-    bool haveWindow = true;         // 是 true、否 false
+
+    public int cc = 2000;
+    public float weight = 1500.5f;         // 小數點後面必須加上 f 或 F
+    public string brand = "BMW";           // 文字必須使用雙引號
+    public bool haveWindow = true;         // 是 true、否 false
 }
