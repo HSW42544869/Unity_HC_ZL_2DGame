b5372ccb1324d792c7431af66711ea305d6dac43
KID <50666207+KID421@users.noreply.github.com>
2020-11-21 19:30:41 +0800

    非靜態

---
+++ 2DGame/Assets/Scripts/APINoStatic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class APINoStatic : MonoBehaviour
+{
+    public GameObject objA;
+    public GameObject objB;
+
+    private void Start()
+    {
+        // 取得 非靜態 屬性
+        // 遊戲物件 A 的 標籤
+        print(objA.tag);
+        print(objB.layer);
+    }
+}
