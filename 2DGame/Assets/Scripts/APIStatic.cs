944caa3ba02ffc19532f139638ba61260cfec4c9
KID <50666207+KID421@users.noreply.github.com>
2020-11-21 17:00:21 +0800

    示範靜態方法

--- 2DGame/Assets/Scripts/APIStatic.cs
+++ 2DGame/Assets/Scripts/APIStatic.cs
@@ -26,16 +26,28 @@ public class APIStatic : MonoBehaviour
         float atk = Random.Range(50.5f, 100.5f);
         print("隨機攻擊力：" + atk);
 
-        // 練習
+        // 練習 屬性
+        // 屬性 1. 攝影機總數
         print("攝影機總數：" + Camera.allCamerasCount);
-
+        // 屬性 2. 隱藏滑鼠
         Cursor.visible = false;
+
+        // 練習 方法
+        // 方法 1. 開啟連結
+        //Application.OpenURL("https://store.steampowered.com/");
+        // 方法 2. 去小數點
+        float number = Mathf.Floor(1.23456789f);
+        print("去小數點：" + number);
     }
 
     private void Update()
     {
+        // 屬性 3. 是否按任意鍵
         print("是否按住任意鍵：" + Input.anyKey);
+        // 屬性 4. 遊戲時間
+        //print("遊戲時間：" + Time.time);
 
-        print("遊戲時間：" + Time.time);
+        // 方法 3. 是否按下空白鍵
+        print("是否按下空白鍵：" + Input.GetKeyDown("space"));
     }
 }
