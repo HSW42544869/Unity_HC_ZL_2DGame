b6fa14cb2c8fe63282052d89109440cc730e5a3d
KID <50666207+KID421@users.noreply.github.com>
2020-12-05 16:27:45 +0800

    追蹤與攻擊時停下來

--- 2DGame/Assets/Scripts/Enemy.cs
+++ 2DGame/Assets/Scripts/Enemy.cs
@@ -3,13 +3,8 @@
 public class Enemy : MonoBehaviour
 {
     #region 欄位
-    // 移動速度
     [Header("移動速度"), Range(0, 1000)]
     public float speed = 10.5f;
-    // 子彈
-    // 子彈生成點
-    // 子彈速度
-    // 開槍音效
     [Header("子彈"), Tooltip("存放要生成的子彈預製物")]
     public GameObject bullet;
     [Header("子彈生成點"), Tooltip("子彈要生成的起始位置")]
@@ -18,15 +13,15 @@ public class Enemy : MonoBehaviour
     public int speedBullet = 800;
     [Header("開槍音效")]
     public AudioClip soundFire;
-    // 追蹤範圍
     [Header("追蹤範圍"), Range(0, 1000)]
     public float rangeTrack = 10.5f;
-    // 攻擊範圍
     [Header("攻擊範圍"), Range(0, 1000)]
     public float rangeAttack = 8.5f;
 
     private Transform player;
     #endregion
+
+    private Rigidbody2D rig;
 
     #region 方法
     /// <summary>
@@ -50,12 +45,13 @@ public class Enemy : MonoBehaviour
         // 如果 距離 < 攻擊：攻擊
         if (dis < rangeAttack)
         {
-            print("進入攻擊範圍");
+            Fire();
         }
         // 否則 距離 < 追蹤：追蹤
         else if(dis < rangeTrack)
         {
-            print("進入追蹤範圍");
+            rig.velocity = transform.right * speed;
+            rig.velocity = new Vector2(rig.velocity.x, rig.velocity.y);
         }
     }
 
@@ -64,7 +60,7 @@ public class Enemy : MonoBehaviour
     /// </summary>
     private void Fire()
     {
-
+        rig.velocity = Vector3.zero;
     }
 
     /// <summary>
@@ -88,6 +84,8 @@ public class Enemy : MonoBehaviour
 
     private void Awake()
     {
+        rig = GetComponent<Rigidbody2D>();
+
         // 玩家變形 = 遊戲物件.尋找("玩家物件名稱").變形
         player = GameObject.Find("玩家").transform;
     }
