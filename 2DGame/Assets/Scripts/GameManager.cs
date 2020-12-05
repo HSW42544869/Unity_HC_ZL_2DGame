5370668bec3156e8f10fc6560a251896fd9ce79f
KID <50666207+KID421@users.noreply.github.com>
2020-12-05 20:26:44 +0800

    使用陣列儲存多個物件並新增文字欄位

--- 2DGame/Assets/Scripts/GameManager.cs
+++ 2DGame/Assets/Scripts/GameManager.cs
@@ -1,7 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;   // 引用 介面 API
 
+/// <summary>
+/// 遊戲管理器：管理生命、分數
+/// </summary>
 public class GameManager : MonoBehaviour
 {
+    // 陣列
+    [Header("生命物件陣列")]
+    public GameObject[] live;
+    [Header("分數文字介面")]
+    public Text textScore;
+
     private void Awake()
     {
         SetCollision();
@@ -12,6 +22,7 @@ public class GameManager : MonoBehaviour
     /// </summary>
     private void SetCollision()
     {
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("敵人"), LayerMask.NameToLayer("敵人"));
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("玩家"), LayerMask.NameToLayer("玩家子彈"));
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("敵人"), LayerMask.NameToLayer("敵人子彈"));
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("玩家子彈"), LayerMask.NameToLayer("敵人子彈"));
