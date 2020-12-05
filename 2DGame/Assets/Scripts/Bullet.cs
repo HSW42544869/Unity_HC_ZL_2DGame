5e1847c196d2001d57ff99ce506ba0305c55ec4c
KID <50666207+KID421@users.noreply.github.com>
2020-12-05 19:49:43 +0800

    子彈碰撞後並刪除

---
+++ 2DGame/Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("爆炸特效")]
+    public GameObject explosion;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+        GameObject temp = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(temp, 1.5f);
+    }
+}
